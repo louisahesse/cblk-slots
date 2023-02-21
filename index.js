b841ce34b972c4813ac0cd4cc1760d4397198199
louisahesse <louisahesse09@gmail.com>
2023-02-21 14:37:46 +0000

    changed position of slots

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function toggleAnswer(id) {
-  var question = document.getElementsByClassName("faq-question")[id - 1];
-  var answer = document.getElementById("answer" + id);
-  question.classList.toggle("open");
-  answer.classList.toggle("show");
+  const answer = document.getElementById("answer" + id);
+  if (answer.style.display === "none") {
+    answer.style.display = "block";
+  } else {
+    answer.style.display = "none";
+  }
 }
